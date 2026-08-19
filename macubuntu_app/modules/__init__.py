5bf958f73131b9e60b4989b3c8c1278576a6f1cf
Fra <40823974+Frapo78@users.noreply.github.com>
2026-08-19 04:55:36 +0200

    feat: switch default appearance to MacTahoe v0.5

--- macubuntu_app/modules/__init__.py
+++ macubuntu_app/modules/__init__.py
@@ -1,4 +1,4 @@
-from .appearance_whitesur import AppearanceWhiteSurModule
+from .appearance_mactahoe import AppearanceMacTahoeModule
 from .core_gnome import CoreGnomeModule
 from .desktop_tools import DesktopToolsModule
 from .gestures_x11 import GesturesX11Module
@@ -7,14 +7,14 @@ from .sharing_warpinator import SharingWarpinatorModule
 from .shell_extensions import ShellExtensionsModule
 from .spotlight_ulauncher import SpotlightUlauncherModule
 from .typography import TypographyModule
-from .wallpaper_whitesur import WallpaperWhiteSurModule
+from .wallpaper_macos import WallpaperMacCollectionModule
 
 ALL_MODULES = [
     CoreGnomeModule(),
     DesktopToolsModule(),
     TypographyModule(),
-    AppearanceWhiteSurModule(),
-    WallpaperWhiteSurModule(),
+    AppearanceMacTahoeModule(),
+    WallpaperMacCollectionModule(),
     ShellExtensionsModule(),
     GesturesX11Module(),
     SpotlightUlauncherModule(),
